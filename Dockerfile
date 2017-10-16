541daea8f97960da658b7f5687327c473ac05a56
Matthew Campbell <hyper@hyperworks.nu>
2017-10-17 00:04:52 +0700

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM scratch
 
 ADD dashboard /dashboard
 ADD static /static
+ADD templates /templates
 
 CMD ["/dashboard"]
--- jenkins.sh
+++ jenkins.sh
@@ -19,7 +19,7 @@ echo "sending $DOC_IMAGE"
 docker build -t $DOC_IMAGE -f Dockerfile .
 docker push $DOC_IMAGE
 
-IMAGE=dashboard
+export IMAGE=dashboard
 
 
-ssh root@128.199.83.146 "docker pull loomnetwork/${IMAGE}:$REV && docker stop ${IMAGE}; true && docker rm ${IMAGE}; true && docker run  --log-driver=syslog -d -v /dev/log:/dev/log -e \"LETSENCRYPT_HOST=dashboard.loomx.io\" -e \"LETSENCRYPT_EMAIL=team@loomx.com\" -e \"VIRTUAL_HOST=dashboard.loomx.io\" -p 8080:8080 --name ${IMAGE} loomnetwork/${IMAGE}:$REV && docker update --restart=always ${IMAGE}"
+ssh root@128.199.83.146 "docker pull loomnetwork/${IMAGE}:$REV && docker stop ${IMAGE}; true && docker rm ${IMAGE}; true && docker run  --log-driver=syslog -d -v /dev/log:/dev/log -e AUTOMIGRATE=1 -e DATABASE_PASS=power2edit -e \"LETSENCRYPT_HOST=dashboard.loomx.io\" -e \"LETSENCRYPT_EMAIL=team@loomx.com\" -e \"VIRTUAL_HOST=dashboard.loomx.io\" -p 8080:8080 --name ${IMAGE} loomnetwork/${IMAGE}:$REV && docker update --restart=always ${IMAGE}"

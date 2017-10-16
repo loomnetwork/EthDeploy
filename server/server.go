8c6b780ff62415537bfac3eaa7317ae371b9939b
Matthew Campbell <hyper@hyperworks.nu>
2017-10-16 17:31:19 +0700

    fix asset

--- server/server.go
+++ server/server.go
@@ -4,12 +4,16 @@ import (
 	"github.com/loomnetwork/dashboard/middleware"
 	"github.com/loomnetwork/dashboard/router"
 
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+
 	"github.com/jinzhu/gorm"
 )
 
 func Setup(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
+	s := static.Serve("/static", static.LocalFile("static", true))
+	r.Use(s)
 	r.Use(middleware.SetDBtoContext(db))
 	router.Initialize(r)
 	return r

9de32b54e145006682ab974a5f2d16074b0cb7dd
Matthew Campbell <hyper@hyperworks.nu>
2017-10-20 18:43:18 +0700

    fix oauth linkedin on prod

--- jenkins.sh
+++ jenkins.sh
@@ -22,5 +22,5 @@ docker push $DOC_IMAGE
 export IMAGE=dashboard
 
 export sshcommand="docker pull loomnetwork/${IMAGE}:$REV && docker stop ${IMAGE}; true && docker rm ${IMAGE}; true && docker run  --log-driver=syslog -d -v /dev/log:/dev/log -e SERVER_HOST=\"https://dashboard.loomx.io\" -e DEMO_MODE=false -e BETA_MODE=false  -e GIN_MODE=debug -e LOG_LEVEL=debug  -e AUTOMIGRATE=1 -e DATABASE_PASS=power2edit -e \"LETSENCRYPT_HOST=dashboard.loomx.io\" -e \"LETSENCRYPT_EMAIL=team@loomx.com\" -e \"VIRTUAL_HOST=dashboard.loomx.io\" -e DATABASE_HOST=172.17.0.1 -p 8081:8080 --name ${IMAGE} loomnetwork/${IMAGE}:$REV && docker update --restart=always ${IMAGE}"
-echo sshcommand
-ssh root@128.199.83.146 sshcommand
+echo $sshcommand
+ssh root@128.199.83.146 $sshcommand

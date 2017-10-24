7bf789e35d65300dd7c0d74a224d729ec305f6e0
Luke Zhang <luke@lukezhang.ca>
2017-10-24 21:06:19 +0700

    getting the dashboard to load model - fetch model based on query - remove mock data

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -3,29 +3,35 @@ package controllers
 import (
 	"fmt"
 	"net/http"
-	"time"
+
+	dbpkg "github.com/loomnetwork/dashboard/db"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
 	"github.com/loomnetwork/dashboard/models"
 )
 
 func ApplicationIndex(c *gin.Context) {
-	app1 := &models.Application{
-		Name:         "test123",
-		Slug:         "123",
-		Status:       "Live",
-		LastDeployed: time.Now(),
-	}
-	app2 := &models.Application{
-		Name:         "test123",
-		Slug:         "123",
-		Status:       "Live",
-		LastDeployed: time.Now(),
+	db := dbpkg.DBInstance(c)
+	parameter, err := dbpkg.NewParameter(c, models.Application{})
+	if err != nil {
+		log.WithField("error", err).Warn("Failed retrieving applications")
 	}
 
-	apps := []*models.Application{
-		app1, app2,
+	db, err = parameter.Paginate(db)
+	if err != nil {
+		log.WithField("error", err).Warn("Failed retrieving applications")
 	}
+
+	db = parameter.SetPreloads(db)
+	db = parameter.SortRecords(db)
+	db = parameter.FilterFields(db)
+	apps := []models.Application{}
+
+	if err := db.Where("").Find(&apps).Error; err != nil {
+		log.WithField("error", err).Warn("Failed retrieving applications")
+	}
+
 	c.HTML(http.StatusOK, "dashboard/index", gin.H{
 		"loggedIn":     true,
 		"applications": apps,
@@ -33,11 +39,37 @@ func ApplicationIndex(c *gin.Context) {
 }
 
 func Dashboard(c *gin.Context) {
-	fmt.Printf("got params %s", c.Params)
+	id := c.Param("id")
+
+	// if id == nil {
+	// 	c.Redirect(301, "/")
+	// }
+
+	db := dbpkg.DBInstance(c)
+	parameter, err := dbpkg.NewParameter(c, models.Application{})
+	if err != nil {
+		log.WithField("error", err).Warn("Failed retrieving application for dashboard")
+	}
+
+	db, err = parameter.Paginate(db)
+	if err != nil {
+		log.WithField("error", err).Warn("Failed retrieving application for dashboard")
+	}
+
+	db = parameter.SetPreloads(db)
+	db = parameter.SortRecords(db)
+	db = parameter.FilterFields(db)
+	app := models.Application{}
+
+	if err := db.Where("slug = ?", id).Find(&app).Error; err != nil {
+		log.WithField("error", err).Warn("Failed retrieving application for dashboard")
+	}
+
+	fmt.Printf("got app %s", app)
 
 	c.HTML(http.StatusOK, "dashboard/dashboard", gin.H{
 		"loggedIn": true,
-		"name":     c.Param("id"),
+		"app":      app,
 	})
 }
 

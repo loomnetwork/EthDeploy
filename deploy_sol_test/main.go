5eb4edcaa800510eee0aa798ab255a2d84895b72
Matthew Campbell <hyper@hyperworks.nu>
2017-10-26 21:52:12 +0700

    example of how to deploy a contract with golang

---
+++ deploy_sol_test/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+const keydata = `9e298e03a6d115213d2172b12c470dbefa627bd2f66f4548eddd6a835f7cda7b`
+
+func main() {
+	// Create an IPC based RPC connection to a remote node and an authorized transactor
+	conn, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	keyBytes := common.FromHex(keydata)
+	key := crypto.ToECDSAUnsafe(keyBytes)
+
+	auth := bind.NewKeyedTransactor(key)
+
+	// Deploy a new awesome contract for the binding demo
+	address, tx, token, err := DeployToken(auth, conn, new(big.Int), "Contracts in Go!!!", 0, "Go!")
+	if err != nil {
+		log.Fatalf("Failed to deploy new token contract: %v", err)
+	}
+	fmt.Printf("Contract pending deploy: 0x%x\n", address)
+	fmt.Printf("Transaction waiting to be mined: 0x%x\n\n", tx.Hash())
+
+	// Don't even wait, check its presence in the local pending state
+	time.Sleep(250 * time.Millisecond) // Allow it to be processed by the local node :P
+
+	name, err := token.Name(&bind.CallOpts{Pending: true})
+	if err != nil {
+		log.Fatalf("Failed to retrieve pending name: %v", err)
+	}
+	fmt.Println("Pending name:", name)
+}

c7afe1376b6b9392907cfe9172be81a9249e9732
Matthew Campbell <hyper@hyperworks.nu>
2017-10-28 16:05:35 +0700

    require cgo for now

--- build_gateway_docker.sh
+++ build_gateway_docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 glide install
-CGO_ENABLED=0 GOOS=linux go build -o rpc_proxy cmd/rpc_proxy/rpc_proxy.go
+#CGO_ENABLED=0  #TODO cant disable cgo cause ethereum, need to tweak some other flags
+rm rpc_proxy ; true
+GOOS=linux go build -o rpc_proxy cmd/rpc_proxy/rpc_proxy.go
 
 REV=`git rev-parse --short HEAD`
 DOC_IMAGE=loomnetwork/rpc_gateway:$REV

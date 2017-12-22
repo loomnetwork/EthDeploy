3fd0f985e7492d438e741d0075b370f5764d48fb
Piyush Verma <github@piyushverma.net>
2017-12-22 08:21:48 +0530

    Send required proxy_addr and capture error

--- controllers/upload.go
+++ controllers/upload.go
@@ -62,6 +62,7 @@ func deployToK8s(filename, slug string, cfg *config.Config) error {
 		"PRIVATE_KEY_JSON_PATH": "data.json",
 		"APP_SLUG":              slug,
 		"ETHEREUM_URI":          fmt.Sprintf("http://%v.loomapps.io:8545", slug),
+		"PROXY_ADDR":            fmt.Sprintf("http://%v.loomapps.io:8545", slug),
 	}
 
 	if err := k8s.Install(gateway.Ident, slug, env, cfg); err != nil {

0e27dfaec70718edf2575eb0c874fae89ade4692
Piyush Verma <github@piyushverma.net>
2017-12-25 00:50:05 +0530

    Tests for gateway and Idempotency

--- cmd/helpers/nomad_helper.go
+++ cmd/helpers/nomad_helper.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"github.com/loomnetwork/dashboard/controllers"
-)
-
 func main() {
-	controllers.SendNomadJob("a4200d60-8817-44a5-92a0-5fe6de44e563.zip", "blockssh")
+	//controllers.SendNomadJob("a4200d60-8817-44a5-92a0-5fe6de44e563.zip", "blockssh")
 }
--- k8s/gateway_deployment.go
+++ k8s/gateway_deployment.go
@@ -1,13 +1,16 @@
 package k8s
 
 import (
-	"github.com/pkg/errors"
-	"k8s.io/api/apps/v1beta2"
-	apiv1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 	"strings"
+
+	"github.com/pkg/errors"
+
+	"k8s.io/api/apps/v1beta2"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/client-go/kubernetes"
+
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // How many pods should be created for this service.
@@ -18,41 +21,18 @@ const (
 	gatewayCPULimit = "5300m"
 )
 
-func (g *GatewayInstaller) createDeployment(slug string, env map[string]interface{}, client *kubernetes.Clientset) error {
-	dClient := client.AppsV1beta2().Deployments(apiv1.NamespaceDefault)
-
-	//check if deployment exists
-	d, err := g.getDeployment(makeGatewayName(slug), client)
-	if d != nil {
-		if env != nil {
-			for i := 0; i < len(d.Spec.Template.Spec.Containers); i++ {
-				d.Spec.Template.Spec.Containers[i].Env = makeEnv(env)
-			}
-
-			if _, err := dClient.Update(d); err != nil {
-				return errors.Wrap(err, "Update to deployment failed.")
-			}
-		}
-
-		return nil
-	}
-
-	if !strings.Contains(err.Error(), "not found") {
-		return errors.Wrap(err, "Error in checking if deployment exists.")
-	}
-
+func (g *GatewayInstaller) createDeploymentStruct(slug string, env map[string]interface{}, client *kubernetes.Clientset) (*v1beta2.Deployment, error) {
 	zone, err := g.getZone(slug, client)
 	if err != nil {
-		return errors.Wrap(err, "Cannot select zone")
+		return nil, errors.Wrap(err, "Cannot select zone")
 	}
 
 	image, err := g.getImage()
 	if err != nil {
-		return errors.Wrap(err, "Cannot get Image")
+		return nil, errors.Wrap(err, "Cannot get Image")
 	}
 
-	//Create deployment definition
-	deployment := &v1beta2.Deployment{
+	d := &v1beta2.Deployment{
 		TypeMeta: metav1.TypeMeta{
 			Kind: "Deployment",
 		},
@@ -105,6 +85,32 @@ func (g *GatewayInstaller) createDeployment(slug string, env map[string]interfac
 				},
 			},
 		},
+	}
+
+	return d, nil
+}
+
+func (g *GatewayInstaller) createDeployment(slug string, env map[string]interface{}, client *kubernetes.Clientset) error {
+	dClient := client.AppsV1beta2().Deployments(apiv1.NamespaceDefault)
+
+	//Create deployment definition
+	deployment, err := g.createDeploymentStruct(slug, env, client)
+	if err != nil {
+		return errors.Wrap(err, "Cannot create deployment request.")
+	}
+
+	//check if deployment exists
+	d, err := g.getDeployment(makeGatewayName(slug), client)
+	if d != nil {
+		if _, err := dClient.Update(d); err != nil {
+			return errors.Wrap(err, "Deployment update failed.")
+		}
+
+		return nil
+	}
+
+	if !strings.Contains(err.Error(), "not found") {
+		return errors.Wrap(err, "Cannot get deployment.")
 	}
 
 	if _, err := dClient.Create(deployment); err != nil {

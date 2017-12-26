f6b21f85663470d37899968249f684ffe808c632
Piyush Verma <github@piyushverma.net>
2017-12-26 17:50:45 +0530

    Bump up test image

--- k8s/installer_test.go
+++ k8s/installer_test.go
@@ -22,7 +22,7 @@ var gwi gateway.Installer
 
 const (
 	slug                 = "hello-world"
-	gatewayDockerVersion = "gcr.io/robotic-catwalk-188706/rpc_gateway:5728a1b"
+	gatewayDockerVersion = "gcr.io/robotic-catwalk-188706/rpc_gateway:a3e7508"
 	ganacheDockerVersion = "gcr.io/robotic-catwalk-188706/loom-ganache:5a4cfce"
 )
 

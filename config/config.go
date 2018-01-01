a8f41532242f83050153565c830948a26c476972
Chinmay Naik <chinmay@oogway.in>
2018-01-01 08:53:44 +0530

    These configurations have moved to rpc-proxy

--- config/config.go
+++ config/config.go
@@ -53,26 +53,17 @@ type RPCConfig struct {
 }
 
 var (
-	proxyAddr          = envflag.String("PROXY_ADDR", "http://localhost:8545", "Where the actual web3 rpc address exists")
-	privateKeyJsonFile = envflag.String("PRIVATE_KEY_JSON_PATH", "data.json", "TestRPC json output")
-	spawnNetwork       = envflag.String("SPAWN_NETWORK", "node tmp/testrpc/build/cli.node.js", "How does test rpc spawn the testrpc or ethereum network")
-	tmpDir             = envflag.String("TMP_DIR", "tmp_uploads", "the directory where we will store the uploaded zip")
-	appSlug            = envflag.String("APP_SLUG", "block_ssh", "domain slug for the application")
 	demo               = envflag.Bool("DEMO_MODE", true, "Enable demo mode for investors, or local development")
 	enableAuth         = envflag.Bool("ENABLE_AUTH", true, "Enables/Disables auth for development")
 	loomEnv            = envflag.String("LOOM_ENV", "devlopment", "devlopment/staging/production")
 	bindAddr           = envflag.String("BIND_ADDR", ":8081", "What address to bind the main webserver to")
-	applicationZipPath = envflag.String("APP_ZIP_FILE", "misc/block_ssh.zip", "Location of app zip file. Relative or on s3 or Digitalocean bucket. Ex. do://uploads/block_ssh.zip")
-	enableFakeData     = envflag.Bool("ENABLE_FAKE_DATA", false, "Stubs out data")
 	inviteOnlyMode     = envflag.Bool("INVITE_ONLY_MODE", true, "Whitelisted users can login")
 	disableUpload      = envflag.Bool("DISABLE_UPLOAD", false, "Doesn't upload to s3 or nomad. Maybe in future we store to local disk?")
 	level              = envflag.String("LOG_LEVEL", "debug", "Log level minimum to output. Info/Debug/Warn")
 	serverHost         = envflag.String("SERVER_HOST", "http://127.0.0.1:8081", "hostname for oauth redirects")
-	loomDashboardHost  = envflag.String("LOOM_DASHBOARD_API_HOST", "https://dashboard.loomx.io", "hostname for production dashboard to read data from it, for the gateway.")
 	gatewayTLD         = envflag.String("GATEWAY_TLD", DefaultGatewayTld, "Default top level domain for gateway, loomapps.io")
 	gatewayDockerImage = envflag.String("GATEWAY_DOCKER_IMAGE", DefaultGatewayImage, "Gateway docker image version")
 	ganacheDockerImage = envflag.String("GANACHE_DOCKER_IMAGE", DefaultGanacheImage, "Ganache docker image version")
-	ethereumURI        = envflag.String("ETHEREUM_URI", "http://localhost:8545", "Test RPC Host address")
 	kubeConfigPath     = envflag.String("KUBECONFIFG", "~/.kube/config", "Path to K8s configuration file")
 )
 
@@ -118,22 +109,6 @@ func GetDefaultedConfig() *Config {
 		InviteOnlyMode:     *inviteOnlyMode,
 		KubeConfigPath:     *kubeConfigPath,
 		S3:                 &S3EndPoint{AccessKeyID: accessKeyID, SecretAccessKey: secretAccessKey, EndPointUrl: endpoint}}
-}
-
-func GetDefaultedRPCConfig() *RPCConfig {
-	cfg := GetDefaultedConfig()
-	return &RPCConfig{
-		ProxyAddr:          *proxyAddr,
-		PrivateKeyJsonFile: *privateKeyJsonFile,
-		SpawnNetwork:       *spawnNetwork,
-		TmpDir:             *tmpDir,
-		ApplicationZipPath: *applicationZipPath,
-		EnableFakeData:     *enableFakeData,
-		LoomDashboardHost:  *loomDashboardHost,
-		AppSlug:            *appSlug,
-		EthereumURI:        *ethereumURI,
-		Config:             cfg,
-	}
 }
 
 //Finding the config on the gin context

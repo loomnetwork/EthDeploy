7df7ab415e1eaf3a7493b441a2548f389a39e8b5
Nishant Modak <modak.nishant@gmail.com>
2018-01-05 11:35:12 +0530

    update jenkins sh to use managed mysql

--- jenkins.sh
+++ jenkins.sh
@@ -13,7 +13,6 @@ npm install
 ./node_modules/.bin/gulp
 
 REV=`git rev-parse --short HEAD`
-DOC_IMAGE=loomnetwork/dashboard:$REV
 DOC_IMAGE=gcr.io/robotic-catwalk-188706/dashboard:${REV}
 
 echo "sending $DOC_IMAGE"
@@ -23,19 +22,13 @@ gcloud docker -- push ${DOC_IMAGE}
 
 export IMAGE=dashboard
 
-#export sshcommand="docker pull loomnetwork/${IMAGE}:$REV && docker stop ${IMAGE}; true && docker rm ${IMAGE}; true && docker run  --log-driver=syslog -d -v /dev/log:/dev/log -e NOMAD_ADDR=http://159.89.198.88:4646 -e BIND_ADDR=\":8080\" -e LOOM_ENV=production -e SERVER_HOST=\"https://dashboard.loomx.io\" -e DEMO_MODE=false -e BETA_MODE=false  -e GIN_MODE=debug -e LOG_LEVEL=debug  -e AUTOMIGRATE=1 -e DATABASE_PASS=power2edit -e \"LETSENCRYPT_HOST=dashboard.loomx.io\" -e \"LETSENCRYPT_EMAIL=team@loomx.com\" -e \"VIRTUAL_HOST=dashboard.loomx.io\" -e DATABASE_HOST=172.17.0.1 -p 8081:8080 --name ${IMAGE} loomnetwork/${IMAGE}:$REV && docker update --restart=always ${IMAGE}"
-#echo $sshcommand
-#ssh root@128.199.83.146 $sshcommand
+#deploy to k8s
 
-#deploy to nomad
+K8S_TMP_FILE=k8s_deployment.yaml
 
-#TMP_FILE=tmp_dashboard.nomad
-#cp dashboard.nomad $TMP_FILE
-#sed -i 's/REV_REPLACE/'"$REV"'/g' $TMP_FILE
-#cat $TMP_FILE
-#NOMAD_ADDR=http://45.55.246.200:4646 nomad run $TMP_FILE
-
-
-echo "Ok now building the RPC Gateway, we don't deploy it anyway automatically yet"
-#todo maybe have two jenkins jobs??
-./build_proxy_docker.sh
+if [[ "$GIT_BRANCH" = "origin/staging" ]]
+then
+    cp k8s/staging.yaml $K8S_TMP_FILE
+    sed -i 's/REV_REPLACE/'"$REV"'/g' $K8S_TMP_FILE
+    kubectl replace -f $K8S_TMP_FILE --kubeconfig=/var/lib/jenkins/staging_kube_config.yaml --force
+fi
